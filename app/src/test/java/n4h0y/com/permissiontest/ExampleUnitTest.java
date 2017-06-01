2f1da746a6dc45ee9a5244be8f2a1c017c26b6d0
Yohan <yohan@uber.com>
2017-05-31 20:58:01 -0700

    Demonstrate that uses-permission-sdk-23 does not respect tools:node=remove
    In the app/src/main/AndroidManifest.xml, we declare <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" tools:node="remove"/> and <uses-permission-sdk-23 android:name="android.permission.ACCESS_COARSE_LOCATION"tools:node="remove"/>
    
    and in the permissionLibrary/main/AndroidManifest.xml we declare <uses-permission-sdk-23 android:name="android.permission.ACCESS_COARSE_LOCATION"/> and <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
    
    However in the merged Manifest file in the apk, <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
    and <uses-permission-sdk-23 android:name="android.permission.ACCESS_COARSE_LOCATION" /> are included so it seems that uses-permission-sdk-23 ignore tools:node="remove"

---
+++ app/src/test/java/n4h0y/com/permissiontest/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package n4h0y.com.permissiontest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
